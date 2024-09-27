df4f34e61d85179e500a6c6c247b14785c835673
abc2962 <abc2962@email.com>
2024-09-26 21:29:26 -0400

    improved

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
-
 const textInput = document.querySelector("#textInput")
 const temp = document.querySelector("#temp")
 const button = document.querySelector("#submitButton")
 const cityName = document.querySelector("#cityName")
-const apiKey = `dca695368ce3428fa84215347242609`
-
-
- 
+const apiKey = `dca695368ce3428fa84215347242609` 
 
 button.addEventListener("click", async () => {
     let city = textInput.value
+    city = textInput.value.toLowerCase()
+    
     let response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`)
-    console.log(response)
-
 let locationArray =  [`${response.data.location}, `, `${response.data.location.region}`]
-locationArray.filter((location, locationArray) => {
-    // if (location === `,` ) {
-    //     locationArray.delete(locationArray[1])
-    //     let locationText = locationArray.join(``)
-        console.log(locationArray)
+locationArray.filter((location, locationArray) => {    
         cityName.textContent = `${response.data.location.name}, ${response.data.location.country}`
-        temp.textContent = `(${response.data.current.temp_f}°F) (${response.data.current.temp_c}°)`
-
-    })
-})
-
-// })
-
+        temp.textContent = `(${response.data.current.temp_f}°F) (${response.data.current.temp_c}°C)`
+    })})
 // dca695368ce3428fa84215347242609
